7d069a55b3465d289fa8069de26bf53e14659ed8
alzey73 <seozoglu@gmail.com>
2023-11-01 19:57:24 +0300

    31.10.23

--- configs/socketConfig.js
+++ configs/socketConfig.js
@@ -1,22 +1,37 @@
-const socketIo=require("socket.io");
+const socketIo = require("socket.io");
+const Message = require("../Models/messagesModel");
 
-module.exports=(server)=>{
+module.exports = (server) => {
     const io = socketIo(server, {
         cors: {
             origin: "*",  // Tüm kökenlere izin verir. İhtiyacınıza göre bu değeri değiştirebilirsiniz.
             methods: ["GET", "POST"]
         }
     });
-    
 
-    io.on("connection",(socket)=>{
-        console.log("Someone connected : ",socket.id);
 
-        socket.on("disconnect",()=>{
-            console.log("user disconnected : ",socket.id);
+    io.on("connection", (socket) => {
+        console.log("Someone connected : ", socket.id);
+
+        // Kullanıcının mesaj göndermesi
+        socket.on('sendMessage', (message) => {
+            // Mesajı veritabanına kaydet
+            const newMessage = new Message({
+                sender: message.sender,
+                receiver: message.receiver,
+                text: message.text
+            });
+            newMessage.save();
+
+            // Mesajı diğer kullanıcıya ilet
+            socket.broadcast.emit('messageReceived', message);
         });
 
-        // Diğer socket olaylarınızı burada tanımlayabilirsiniz.
+        socket.on("disconnect", () => {
+            console.log("user disconnected : ", socket.id);
+        });
+
+
     });
 
     return io;// Eğer başka yerlerde io nesnesine ihtiyaç duyarsanız.
--- views/scripts/messagepage.js
+++ views/scripts/messagepage.js
@@ -1,13 +1,13 @@
 const socket = io('http://localhost:3000');
 
-socket.on('connect', function() {
+socket.on('connect', function () {
     console.log('Bağlandı:', socket.id);
     // Kullanıcı listesini almak için bir istek yapabilirsiniz.
     // Örneğin: fetch('/api/users').then(...)
     // Dönen kullanıcı listesi ile #userList'i doldurabilirsiniz.
 });
 
-socket.on('messageReceived', function(message) {
+socket.on('messageReceived', function (message) {
     const messagesDiv = document.getElementById('messages');
     messagesDiv.innerHTML += `<p>${message.sender}: ${message.text}</p>`;
 });
@@ -20,26 +20,26 @@ function fetchUserMessages() {
             'x-auth-token': token
         }
     })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(response.statusText);
-        }
-        return response.json();
-    })
-    .then(data => {
-        console.log("Data received from server:", data);
-        const messagesData = data;
-        if (messagesData && messagesData.length > 0) {
-            const messagesDiv = document.getElementById('messages');
-            messagesData.forEach(message => {
-                messagesDiv.innerHTML += `<p>${message.sender}: ${message.text}</p>`;
-            });
-        }
-    })
-    .catch(error => {
-        console.error('Error:', error);
-        document.getElementById("ss").innerText = error.message;
-    });
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.statusText);
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log("Data received from server:", data);
+            const messagesData = data;
+            if (messagesData && messagesData.length > 0) {
+                const messagesDiv = document.getElementById('messages');
+                messagesData.forEach(message => {
+                    messagesDiv.innerHTML += `<p>${message.sender}: ${message.text}</p>`;
+                });
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            document.getElementById("ss").innerText = error.message;
+        });
 }
 
 function sendMessage() {
@@ -55,6 +55,45 @@ function sendMessage() {
     messageInput.value = '';
 }
 
-window.onload = function() {
+function decodeToken(token) {
+    try {
+        const payload = token.split('.')[1];
+        const decoded = JSON.parse(atob(payload));
+        return decoded;
+    } catch (e) {
+        console.error('Token decode error:', e);
+        return null;
+    }
+}
+
+function fetchUsers() {
+
+    const token2 = localStorage.getItem('token');
+    const decodedToken = decodeToken(token2);
+    const currentUserId = decodedToken.userId;
+
+    fetch('http://localhost:3000/api/users')
+        .then(response => response.json())
+        .then(users => {
+            const userSelect = document.getElementById('userSelect');
+            users.forEach(user => {
+                if (user._id !== currentUserId) {
+                    const option = document.createElement('option');
+                    option.value = user._id; // Kullanıcının ID'si
+                    console.log(option.value);
+                    option.textContent = user.email; // Kullanıcının email adresi veya diğer bir bilgisi
+                    userSelect.appendChild(option);
+                }
+            });
+        })
+        .catch(error => {
+            console.error('Error fetching users:', error);
+        });
+}
+
+
+
+window.onload = function () {
     fetchUserMessages();
+    fetchUsers();
 }
